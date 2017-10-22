51b956d090b6fe8aa6690c1c25206d46dee9a826
Rigidit <rigidit@hotmail.com>
2017-10-22 15:04:24 +0300

    L4 - Decision Trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+acc_min_samples_split = accuracy_score(labels_test, pred)
+print acc_min_samples_split
+
+### Accuracy for min_samples_split=40
+### 0.977815699659
+
+# print len(features_train[0])
+
+### Number of features is 3785
+### Number of features if we set selector = SelectPercentile(f_classif, percentile=1)
+### 379
+### Accuracy for min_samples_split=40 if we set selector = SelectPercentile(f_classif, percentile=1)
+### 0.967007963595
 #########################################################
-
-

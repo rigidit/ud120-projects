1704fb9bf744d3ba6a1095cf7b4e79e5c0d87636
Rigidit <rigidit@hotmail.com>
2017-10-28 17:24:39 +0300

    L7 - Regressions

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "salary"] ### long_term_incentive
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,11 +38,25 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+print "Slope is", reg.coef_
+print "Intercept is", reg.intercept_
+print "Score on the training data is", reg.score(feature_train, target_train)
+print "Score on the test data is", reg.score(feature_test, target_test)
 
+### Results for "salary"
+# Slope is [ 5.44814029]
+# Intercept is -102360.543294
+# Score on the training data is 0.0455091926995
+# Score on the test data is -1.48499241737
 
-
-
-
+### Results for "long_term_incentive"
+# Slope is [ 1.19214699]
+# Intercept is 554478.756215
+# Score on the training data is 0.217085971258
+# Score on the test data is -0.59271289995
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +78,10 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+print "Slope of the new regression line is", reg.coef_
+### Slope of the new regression line is [ 2.27410114]
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
